033cbaf8f939b4c0132aa7588ee0aade4bb610e2
Temirlan Malikov <timalikov@Temirlans-MacBook-Air.local>
2025-03-13 14:36:32 +0500

    feat(all_apps): registered to admin panel

--- places/admin.py
+++ places/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
 
-# Register your models here.
+from places.models import City, Country, Hotel, Place, PlaceCategory
+
+admin.site.register(Place)
+admin.site.register(PlaceCategory)
+admin.site.register(Country)
+admin.site.register(City)
+admin.site.register(Hotel)
--- trips/admin.py
+++ trips/admin.py
@@ -1,3 +1,5 @@
 from django.contrib import admin
 
-# Register your models here.
+from trips.models import UserTrip
+
+admin.site.register(UserTrip)
